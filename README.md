e466412344dd3bc9abb8d93689a3c908c6f797a6
John A. De Goes <john@degoes.net>
2015-01-16 16:28:25 -0700

    first stab at project sketch

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # purescript-echarts
+
+Purescript bindings for Baidu's [Enterprise Charts (Echarts) charting library](https://ecomfe.github.io/echarts/doc/doc-en.html).
+
+**Note**: *SlamData is offering a $2K bounty to whoever authors the first Pull Request that we merge into this repository. This is a great way to learn some Purescript, as well as contribute to a 100% open source library released under a commercial-friendly MIT license.*
+
+## API
+
+The API is a low-level, type-safe, straightforward binding to Echarts:
+
+* All effectful Javascript functions (i.e. virtually *all* functions!) are wrapped in the `Eff` monad for the appropriate effect type (`random`, `dom`, etc.).
+* All Javascript parameter or return values which may be nullable are wrapped / unwrapped using `Maybe`.
+* Purescript record types are used for Javascript objects.
+* Javascript's fake sum types are mapped to Purescript's real sum types, which necessitates encoding / decoding.
+* Where Echarts accepts arbitrary JSON, Argonaut JSON data types are used (but records are used to capture as much structure as possible).
+
+## Examples
+
+The `examples` directory contains an `index.html`, which contains a demonstration of each of the 12 different chart types, showing how the Purescript Echarts API may be used.
---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+'use strict'
+
+var gulp        = require('gulp')
+  , purescript  = require('gulp-purescript')
+  , run         = require('gulp-run')
+  , runSequence = require('run-sequence')
+  , jsValidate  = require('gulp-jsvalidate')
+  ;
+
+var paths = {
+    src: 'src/**/*.purs',
+    bowerSrc: [
+      'bower_components/purescript-*/src/**/*.purs',
+      'bower_components/purescript-*/src/**/*.purs.hs'
+    ],
+    dest: '',
+    docs: {
+        'all': {
+            dest: 'MODULES.md',
+            src: [
+              'src/**/*.purs'
+            ]
+        }
+    },
+    exampleSrc: 'example/Main.purs',
+    test: 'test/**/*.purs'
+};
+
+var options = {
+    test: {
+        main: 'Test.Main',
+        output: 'output/test.js'
+    }
+};
+
+function compile (compiler, src, opts) {
+    var psc = compiler(opts);
+    psc.on('error', function(e) {
+        console.error(e.message);
+        psc.end();
+    });
+    return gulp.src(src.concat(paths.bowerSrc))
+        .pipe(psc)
+        .pipe(jsValidate());
+};
+
+function docs (target) {
+    return function() {
+        var docgen = purescript.docgen();
+        docgen.on('error', function(e) {
+            console.error(e.message);
+            docgen.end();
+        });
+        return gulp.src(paths.docs[target].src)
+            .pipe(docgen)
+            .pipe(gulp.dest(paths.docs[target].dest));
+    }
+}
+
+function sequence () {
+    var args = [].slice.apply(arguments);
+    return function() {
+        runSequence.apply(null, args);
+    }
+}
+
+gulp.task('browser', function() {
+    return compile(purescript.psc, [paths.src].concat(paths.bowerSrc), {})
+        .pipe(gulp.dest('example'))
+});
+
+gulp.task('make', function() {
+    return compile(purescript.pscMake, [paths.src].concat(paths.bowerSrc), {})
+        .pipe(gulp.dest(paths.dest))
+});
+
+gulp.task('test', function() {
+    return compile(purescript.psc, [paths.src, paths.test].concat(paths.bowerSrc), options.test)
+        .pipe(run('node').exec());
+});
+
+gulp.task('docs', docs('all'));
+
+gulp.task('watch-browser', function() {
+    gulp.watch(paths.src, sequence('browser', 'docs'));
+});
+
+gulp.task('watch-make', function() {
+    gulp.watch(paths.src, sequence('make', 'docs'));
+});
+
+gulp.task('default', sequence('make', 'docs', 'browser'));
