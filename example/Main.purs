caf3c64bb04f80a71a4f21c30ae9f7a2b9d54489
Gary Burgess <gary.burgess@gmail.com>
2015-08-25 13:46:10 +0100

    Update dependencies, use purescript-dom

--- example/Main.purs
+++ example/Main.purs
@@ -1,38 +1,42 @@
 module Main where
 
 import Prelude
-import Utils
-import Line4 (line4)
-import Scatter3 (scatter3)
-import K (k)
-import Radar3 (radar3)
+
+import DOM.Node.Types (ElementId(..))
+
 import Chord2 (chord2)
-import Force4 (force4)
-import Map11 (map11)
-import Gauge4 (gauge4)
-import Funnel2 (funnel2) 
-import EventRiver1 (eventRiver) 
-import DynamicLineBar (dynamicLineBar)
-import Loading (loading)
-import Events (events) 
 import Connect (connectM)
+import Control.Monad.Eff (Eff())
+import DynamicLineBar (dynamicLineBar)
+import ECharts.Effects
+import EventRiver1 (eventRiver)
+import Events (events)
+import Force4 (force4)
+import Funnel2 (funnel2)
+import Gauge4 (gauge4)
+import K (k)
+import Line4 (line4)
+import Loading (loading)
+import Map11 (map11)
 import Mix2Safe (mix2safe)
+import Radar3 (radar3)
+import Scatter3 (scatter3)
+import Utils
 
 main = onLoad $ do
-  line4 "line4"
-  scatter3 "scatter3"
-  k "k"
-  radar3 "radar3"
-  chord2 "chord2"
-  force4 "force4"
-  map11 "map11"
-  gauge4 "gauge4"
-  funnel2 "funnel2"
-  eventRiver "event-river"
-  
-  dynamicLineBar "dynamic-line-bar"
-  loading "loading"
-  events "events"
-  connectM "connect1" "connect2"
-  mix2safe "mix2safe"
+  line4 (ElementId "line4")
+  scatter3 (ElementId "scatter3")
+  k (ElementId "k")
+  radar3 (ElementId "radar3")
+  chord2 (ElementId "chord2")
+  force4 (ElementId "force4")
+  map11 (ElementId "map11")
+  gauge4 (ElementId "gauge4")
+  funnel2 (ElementId "funnel2")
+  eventRiver (ElementId "event-river")
 
+  dynamicLineBar (ElementId "dynamic-line-bar")
+  loading (ElementId "loading")
+  events (ElementId "events")
+  connectM (ElementId "connect1") (ElementId "connect2")
+  mix2safe (ElementId "mix2safe")
--- example/Utils.purs
+++ example/Utils.purs
@@ -1,47 +1,54 @@
 module Utils where
 
 import Prelude
+
 import Control.Monad.Eff
 import Control.Monad.Eff.Random
-import DOM
-import Math (floor, round, pow)
+import Control.Bind ((=<<), (<=<))
+
 import Data.Array
-import Data.Maybe
-import Data.Tuple
 import Data.Int (fromNumber, toNumber)
-import qualified Data.DOM.Simple.Window as Win
-import qualified Data.DOM.Simple.Document as Doc
-import qualified Data.DOM.Simple.Element as El
-import qualified Data.DOM.Simple.Events as Ev
-import Data.DOM.Simple.Types (DOMEvent(..))
+import Data.Maybe
+import Data.Either (either)
+import Data.Tuple
+import Data.Nullable (toMaybe)
+import Data.Foreign (toForeign)
 
-getElementById id = do
-  doc <- Win.document Win.globalWindow
-  El.getElementById id doc
+import DOM (DOM())
+import DOM.Event.EventTarget (eventListener, addEventListener)
+import DOM.Event.EventTypes (load)
+import DOM.Event.Types (Event())
+import DOM.HTML (window)
+import DOM.HTML.Document (body)
+import DOM.HTML.Types (HTMLElement(), windowToEventTarget, htmlDocumentToNonElementParentNode, readHTMLElement)
+import DOM.HTML.Window (document)
+import DOM.Node.Node (appendChild)
+import DOM.Node.Types (Element(), ElementId())
+import qualified DOM.Node.NonElementParentNode as NEPN
 
+import Math (floor, round, pow)
 
-onLoad action = 
-  let actionWrap :: DOMEvent -> Eff _ Unit
-      actionWrap _ = action
-  in Ev.addUIEventListener Ev.LoadEvent actionWrap Win.globalWindow
+getElementById :: forall eff. ElementId -> Eff (dom :: DOM | eff) (Maybe HTMLElement)
+getElementById elementId = do
+  win <- window
+  doc <- document win
+  el <- NEPN.getElementById elementId (htmlDocumentToNonElementParentNode doc)
+  pure $ either (const Nothing) Just $ readHTMLElement (toForeign el)
 
-
+onLoad :: forall eff a. Eff (dom :: DOM | eff) a -> Eff (dom :: DOM | eff) Unit
+onLoad handler = addEventListener load (eventListener (const handler)) false <<< windowToEventTarget =<< window
 
 precise :: Number -> Number -> Number
 precise pre num =
-  (round $  (pow 10.0 pre) * num) / (pow 10.0 pre)
+  (round $ (pow 10.0 pre) * num) / (pow 10.0 pre)
 
 -- sequence_ $ traverse (\i -> random) (1..10000) -> stackoverflow
 foreign import randomLst :: forall e.  Number -> Eff (random :: RANDOM|e) (Array Number)
 
-
 randomInList :: forall a e. Array a -> Eff (random :: RANDOM|e) (Tuple a Number)
-randomInList lst = do 
+randomInList lst = do
   let l = length lst
   rnd <- random
   let i = floor (rnd * toNumber l)
   return $ case fromNumber i >>= (lst !!) of
     Just x -> Tuple x i
-
-
-      
