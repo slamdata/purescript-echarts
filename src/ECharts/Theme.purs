c97e7ccfd9eddf0f3080e8fda9b0c46e2aaad935
Irakli Safareli <i.safareli@gmail.com>
2017-03-30 16:05:01 +0400

    add BuiltInTheme type with IsForeign instance

--- src/ECharts/Theme.purs
+++ src/ECharts/Theme.purs
@@ -6,28 +6,55 @@ module ECharts.Theme
   , roma
   , shine
   , vintage
+  , BuiltInTheme(..)
+  , builtInToTheme
   ) where
 
-import Data.Foreign (Foreign)
+import Prelude
 
-foreign import forceExport :: Foreign
+import Data.Foreign (Foreign, ForeignError(..), fail, readString)
+import Data.Foreign.Class (class IsForeign)
+
+foreign import forceExport ∷ Foreign
 
 data Theme = ByName String | FromObject Foreign
+data BuiltInTheme = Infographic | Macarons | Roma | Shine | Vintage | Dark
+
+instance builtInThemeIsForeign ∷ IsForeign BuiltInTheme where
+  read f = do
+    str ← readString f
+    case str of
+      "infographic" → pure Infographic
+      "macarons" → pure Macarons
+      "roma" → pure Roma
+      "shine" → pure Shine
+      "vintage" → pure Vintage
+      "dark" → pure Dark
+      _ → fail $ ForeignError $ "Theme `" <> str <> "` is not supported"
+
+builtInToTheme ∷ BuiltInTheme → Theme
+builtInToTheme = case _ of
+  Infographic → ByName "infographic"
+  Macarons → ByName "macarons"
+  Roma → ByName "roma"
+  Shine → ByName "shine"
+  Vintage → ByName "vintage"
+  Dark → ByName "dark"
 
 dark ∷ Theme
-dark = ByName "dark"
+dark = builtInToTheme Dark
 
 infographic ∷ Theme
-infographic = ByName "infographic"
+infographic = builtInToTheme Infographic
 
 macarons ∷ Theme
-macarons = ByName "macarons"
+macarons = builtInToTheme Macarons
 
 roma ∷ Theme
-roma = ByName "roma"
+roma = builtInToTheme Roma
 
 shine ∷ Theme
-shine = ByName "shine"
+shine = builtInToTheme Shine
 
 vintage ∷ Theme
-vintage = ByName "vintage"
+vintage = builtInToTheme Vintage
